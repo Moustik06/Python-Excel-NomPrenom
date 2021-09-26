2328ffcaec5ca29036f9cf0356f5dae4fb1cab59
Moustik06 <79152815+Moustik06@users.noreply.github.com>
2021-09-26 12:14:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def prenomNom():
     N = []
     P = []
     for i in range(1,sh.nrows):
-            prenom = sh.cell(i,2)
-            nom = sh.cell(i,1)
+            prenom = sh.cell(i,2)#Colonne du prénom
+            nom = sh.cell(i,1)#Colonne
             P.append(str(prenom)[6:-1])
             N.append(str(nom)[6:-1])
-            print(P[i-1],N[i-1],sep=".",end="@xxxxxxx.xx")
+            print(P[i-1],N[i-1],sep=".",end="@xxxxxxx.xx")#Ajoute un émail a la fin
 prenomNom() 
 
